77db30293246022f2cebb0bc2682dffbca3c0357
RUTENCO <daniel.ramirez7@udea.edu.co>
2026-06-11 17:29:33 -0500

    refactor(logout): optimize explicit wait intervals in dashboard task

--- src/main/java/co/edu/udea/certificacion/caso13/caso13/tasks/LogoutFromDashboard.java
+++ src/main/java/co/edu/udea/certificacion/caso13/caso13/tasks/LogoutFromDashboard.java
@@ -1,6 +1,7 @@
 package co.edu.udea.certificacion.caso13.caso13.tasks;
 
 import co.edu.udea.certificacion.caso13.caso13.interactions.HacerClick;
+import co.edu.udea.certificacion.caso13.caso13.interactions.WaitExplicitly;
 import co.edu.udea.certificacion.caso13.caso13.userinterfaces.DashboardPageUI;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
@@ -25,6 +26,8 @@ public class LogoutFromDashboard implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(HacerClick.en(DashboardPageUI.LOGOUT_BUTTON));
+        actor.attemptsTo(
+            WaitExplicitly.forSeconds(2),
+            HacerClick.en(DashboardPageUI.LOGOUT_BUTTON));
     }
 }
